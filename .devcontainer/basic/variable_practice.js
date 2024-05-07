ee16dbaa597002836604e0a1cb14e13ddbf51612
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2024-05-07 07:33:08 +0000

    Stack Depiction

--- .devcontainer/basic/variable_practice.js
+++ .devcontainer/basic/variable_practice.js
@@ -7,3 +7,10 @@ let userName;
 console.table([name, email, accountID,userName]);
 /* Perfer not to use var because of block scope and functional scope issue*/
 // Stack(Primitive) - The change get updated in a copy  Heap(Non-Primitive) - The change gets updated in the actual
+
+let myData = "Atul Mishra";
+let newData = myData;
+console.log(newData);
+newData = "Karan";
+console.log(newData);
+
