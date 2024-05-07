32b585972d20e525d1ca04dd58079843f10e249c
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2024-05-07 09:36:58 +0000

    String_Functions

---
+++ .devcontainer/basic/Strings.js
@@ -0,0 +1,10 @@
+// String
+
+const name1 = "Atul"
+const repoCount = 50
+// console.log(name+repoCount)
+
+console.log(`Hi ${name1}, Your Repo Count is ${repoCount}`);
+
+console.log(name1.indexOf('t'));
+console.log(name1.charAt(2));
