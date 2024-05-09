fdd9a6178b1519a1d709e2ea06f85930c43a3e80
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2024-05-09 05:40:36 +0000

    Spread and Flat Operator

--- .devcontainer/basic/Arrays.js
+++ .devcontainer/basic/Arrays.js
@@ -22,3 +22,10 @@ const array = ["Atul","Ranjit","Kunal"];
 const new_array = ["Pratiksha","Mayuri"];
 const a_new_array = [...array,...new_array];
 console.log(a_new_array); //O/p - ["Atul","Ranjit","Kunal","Pratiksha","Mayuri"]
+
+//flat Operator
+
+const another_array = [1,2,3,4,[5,6],7,[8,9,0],10,[11,12]];
+const new_another_array = another_array.flat(Infinity);
+console.log(new_another_array);
+
