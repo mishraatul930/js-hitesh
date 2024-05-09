b933d150f74892c1546b9d3fdcf3edc11134ee30
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2024-05-09 07:57:18 +0000

    Object Operations

--- .devcontainer/basic/Objects.js
+++ .devcontainer/basic/Objects.js
@@ -29,3 +29,19 @@ JsUser.greetingTwo = function(){
 }
 
 console.log(JsUser.greetingTwo());
+
+
+const obj1 = {1:"a",2:"b",3:"c"};
+const obj2 = {4:"d",5:"e", 6:"f"};
+
+const obj3 = Object.assign(obj1, obj2);
+console.log(obj3);
+
+//We can use spread operator also in Project
+const obj4 = {...obj1, ...obj2}
+console.log(obj4);
+
+//To check for Keys and Values
+console.log(Object.keys(obj1));
+console.log(Object.values(obj2));
+console.log(Object.entries(obj1)); //returns an array containing all the keys and values
