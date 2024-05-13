06472a7971b04c8c4b09b55b8908ed6c763873ab
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2024-05-13 10:28:58 +0000

    Scope Concept

---
+++ .devcontainer/basic/Scopes.js
@@ -0,0 +1,15 @@
+// let a = 10;
+// const b = 20;
+var c = 30;
+
+if(true){
+    let a = 40;
+    const b = 50;
+    var c = 60;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Since the value of c changes even if it is out of the if scope hence using var is not recommended.
